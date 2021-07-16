197bcdc18203345130f8f20a3e63d64580d04b66
agholson <87455899+agholson@users.noreply.github.com>
2021-07-15 22:16:20 -0400

    Initial Commit

---
+++ Lesson2-Challenge/Lesson2_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson2_ChallengeApp.swift
+//  Lesson2-Challenge
+//
+//  Created by Shepherd on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson2_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
